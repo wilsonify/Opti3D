339963fc0b75c193e89eb5c88e18878a247df22e
wilsonify <tom.andrew.wilson@gmail.com>
2019-06-08 14:48:47 -0500

    define a placeholder main

---
+++ src/stldeli/__main__.py
@@ -0,0 +1,19 @@
+"""
+main module of stldeli
+this script runs when you call the module from the command line.
+```
+python -m stldeli
+```
+"""
+
+
+def main():
+    """
+    main
+    :return:
+    """
+    return None
+
+
+if __name__ == '__main__':
+    main()
