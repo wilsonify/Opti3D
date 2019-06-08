cd771c8898d72bf5f24a592be8b768e772be5098
wilsonify <tom.andrew.wilson@gmail.com>
2019-06-08 14:49:41 -0500

    create setup.py

---
+++ src/setup.py
@@ -0,0 +1,17 @@
+"""
+setup for stldeli
+find all the packages in src
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='stldeli',
+    version='0.0.0',
+    packages=find_packages(),
+    package_dir={'': 'src'},
+    url='',
+    license='MIT',
+    author='Tom Wilson',
+    author_email='tom.andrew.wilson@gmail.com',
+    description='slice arbitrary stl file into optimal gcode'
+)
