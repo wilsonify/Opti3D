74d37cbc56fef741aa8d848144e9ecda0bb6fbe0
wilsonify <tom.andrew.wilson@gmail.com>
2019-06-21 21:14:40 -0500

    remove globals

--- src/stldeli/__init__.py
+++ src/stldeli/__init__.py
@@ -3,20 +3,4 @@ initialize the stldeli
 """
 import logging
 
-import pandas as pd
-
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-global evolution_df
-
-evolution_df = pd.DataFrame(columns=['layer_height',
-                                     'fill_density',
-                                     'infill_every_layers',
-                                     'wall_thickness',
-                                     'nozzle_temperature',
-                                     'iteration',
-                                     'cost'
-                                     ])
-
-global iteration
-iteration = 1
